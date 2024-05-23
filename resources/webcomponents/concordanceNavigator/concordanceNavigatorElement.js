e2dc07c16eff09f0d2991b6f099137e5e188fb63
Dennis Friedl <DennisFriedl@paderborn.com>
2024-05-23 15:55:29 +0200

    Anfang GroupSelector

--- resources/webcomponents/concordanceNavigator/concordanceNavigatorElement.js
+++ resources/webcomponents/concordanceNavigator/concordanceNavigatorElement.js
@@ -4,13 +4,20 @@ fetch("resources/webcomponents/concordanceNavigator/concordanceNavigatorElement.
     .then(stream => stream.text())
     .then(text => define(text));
 
+
 function define(html) {
     class concordanceNavigatorElement extends HTMLElement {
         constructor() {
             super();
+            let me = this;
             this.shadow = this.attachShadow({ mode: "open" });
             this.shadow.innerHTML = html;
             this.concordanceSelector = this.shadow.querySelector("#concordance-selector");
+            this.groupSelector = this.shadow.querySelector("#group-selector");
+            this.groupSelectorLabel = this.shadow.querySelector("#group-selector-label");
+            this.concordances = [];
+
+            this.concordanceSelector.addEventListener("change", function () { me.switchConcordance(this.value) });
         }
 
         static get observedAttributes() {
@@ -38,13 +45,44 @@ function define(html) {
 
         }
 
+        // Fill the menu with concordances
         setConcordances = () => {
             this.concordanceSelector.innerHTML = ""; // Clear the select
             for (let concordance of this.concordances) {
                 let option = document.createElement("option");
-                option.value = concordance.id;
+                option.value = concordance.name;
                 option.text = concordance.name;
+                if (concordance == this.concordances[0]) { // Select the first concordance
+                    option.selected = true;
+                }
                 this.concordanceSelector.appendChild(option);
+
+                // Just for testing, delete later
+                let option2 = document.createElement("option");
+                option2.value = "Test1";
+                option2.text = "Test1";
+                this.concordanceSelector.appendChild(option2);
+
+            }
+
+            // if (this.concordances.length > 0) {
+            //     this.switchConcordance();
+            // }
+        }
+
+        switchConcordance = (concordanceName) => {
+            console.log("Concordance switched!");
+            var concordance = this.concordances.find(concordance => concordance.name === concordanceName);
+            console.log(concordance);
+            var hasGroups = concordance.groups != null;
+            console.log(hasGroups);
+
+            // hier dann noch groupvisibility
+
+            if (hasGroups) {
+                this.groupSelectorLabel.innerHTML = concordance.groups.label;
+            } else {
+                console.log("No groups");
             }
         }
 
